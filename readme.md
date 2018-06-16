f83c89b4cf1f174183c4bcf7f4ba5d8c64a72c9d
Vinay Kumar <15pa1a0470@vishnu.edu.in>
2018-06-16 22:46:38 +0530

    intial

---
+++ readme.md
@@ -0,0 +1,30 @@
+## Neighbourhood Map
+This application uses Google Maps, Knockout JS as well as Foursquare api.
+
+This will briefly displays a map with custom stlye
+
+## Libraries, frameworks and APIs used
+1. Knockout JS(For filtering)
+
+2. Google Maps(Maps api)
+
+3. Foursquare(Displaying content)
+
+4. JQuery(ajax)
+
+## Project Requirements / Features
+1. Google Chrome browser recommended.
+
+2. Internet connectin is neccessary.
+
+## How to run?
+1. Clone this repo.
+
+2. Open map.html file in a browser.
+
+3. Internet connection is recommended as it draws data from cloud.
+
+4. Click on markers to expand popups.
+
+## For live this project
+* http://crazyvny.github.io
---
+++ scripts/main.js
@@ -0,0 +1,340 @@
+// Global Variables
+
+function myFunction() {
+    var x = document.getElementById("list");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+var myLocations = [
+    {
+      lat:12.9507,
+      lng:77.5848,
+      //iconImage:'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/67-512.png',
+      title:'Lal Bagh'
+    },
+    {
+      lat:12.9763,
+      lng:77.5929,
+      title:'Cubbon Park',
+      //iconImage:'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/67-512.png',
+    },
+    {
+      lat:12.9795,
+      lng:77.5909,
+      title:'Vidhana Soudha'
+      //iconImage:'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/67-512.png'
+    },
+    {
+        lat:12.9593,
+        lng:77.5736,
+        title:'Tippu Sultan Summer Palace'
+    },
+    {
+       lat:12.9851,
+       lng:77.5916,
+       title:'Indira Gandhi Musical Fountain Park'
+    }
+  ];
+var map, clientID, clientSecret;
+
+function ViewModel() {
+    var self = this;
+
+    this.searchOption = ko.observable("");
+    this.markers = [];
+
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.setContent('');
+            infowindow.marker = marker;
+            // Foursquare API Client
+            clientID = "A2LRO411UBPHYA1OC4BWYUB4Y2YVYHMKCXIDQP4M4YKKN5OC";
+            clientSecret =
+                "JGSSSF5CRKAWSH2XTUMIBJAFFLGRWDZROM0NICJVTU5L21VA";
+            // URL for Foursquare API
+            var apiUrl = 'https://api.foursquare.com/v2/venues/search?ll=' +
+                marker.lat + ',' + marker.lng + '&client_id=' + clientID +
+                '&client_secret=' + clientSecret + '&query=' + marker.title +
+                '&v=20170708' + '&m=foursquare';
+            // Foursquare API
+            $.getJSON(apiUrl).done(function(marker) {
+                var response = marker.response.venues[0];
+                self.street = response.location.formattedAddress[0] || 'No Street Found';
+                self.city = response.location.formattedAddress[1] || 'No City Found';
+                //self.zip = response.location.formattedAddress[3] || 'No Zip Found';
+                self.country = response.location.formattedAddress[4] || 'No country Found';
+                self.category = response.categories[0].shortName;
+
+                self.htmlContentFoursquare =
+                    '<h5 class="sub_title">(' + self.category +
+                    ')</h5>' + '<div>' +
+                    '<h6 class="address_title"> Address: </h6>' +
+                    '<p class="address">' + self.street + '</p>' +
+                    '<p class="address">' + self.city + '</p>' +
+                    
+                    '<p class="address">' + self.country +
+                    '</p>' + '</div>' + '</div>';
+
+                infowindow.setContent(self.htmlContent + self.htmlContentFoursquare);
+            }).fail(function() {
+                // Send alert
+                alert(
+                    "There was an issue loading the Foursquare API. Please refresh your page to try again."
+                );
+            });
+
+            this.htmlContent = '<div>' + '<h4 class="title">' + marker.title +
+                '</h4>';
+
+            infowindow.open(map, marker);
+
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+        }
+    };
+
+    this.populateAndBounceMarker = function() {
+        self.populateInfoWindow(this, self.largeInfoWindow);
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout((function() {
+            this.setAnimation(null);
+        }).bind(this), 1400);
+    };
+
+    this.initMap = function() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(12.9716, 77.5946),
+            zoom: 13,
+            styles: [
+                {
+                    "featureType": "all",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "saturation": 36
+                        },
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 40
+                        }
+                    ]
+                },
+                {
+                    "featureType": "all",
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 16
+                        }
+                    ]
+                },
+                {
+                    "featureType": "all",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 20
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 17
+                        },
+                        {
+                            "weight": 1.2
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 20
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 21
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 17
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 29
+                        },
+                        {
+                            "weight": 0.2
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 18
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 16
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 19
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#000000"
+                        },
+                        {
+                            "lightness": 17
+                        }
+                    ]
+                }
+            ],
+        };
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(mapCanvas, mapOptions);
+
+        // Set InfoWindow
+        this.largeInfoWindow = new google.maps.InfoWindow();
+        for (var i = 0; i < myLocations.length; i++) {
+            this.markerTitle = myLocations[i].title;
+            this.markerLat = myLocations[i].lat;
+            this.markerLng = myLocations[i].lng;
+            // Google Maps marker setup
+            this.marker = new google.maps.Marker({
+                map: map,
+                position: {
+                    lat: this.markerLat,
+                    lng: this.markerLng
+                },
+                title: this.markerTitle,
+                lat: this.markerLat,
+                lng: this.markerLng,
+                id: i,
+                animation: google.maps.Animation.DROP
+            });
+            this.marker.setMap(map);
+            this.markers.push(this.marker);
+            this.marker.addListener('click', self.populateAndBounceMarker);
+        }
+    };
+
+    this.initMap();
+
+    // This block appends our locations to a list using data-bind
+    // It also serves to make the filter work
+    this.myLocationsFilter = ko.computed(function() {
+        var result = [];
+        for (var i = 0; i < this.markers.length; i++) {
+            var markerLocation = this.markers[i];
+            if (markerLocation.title.toLowerCase().includes(this.searchOption()
+                    .toLowerCase())) {
+                result.push(markerLocation);
+                this.markers[i].setVisible(true);
+            } else {
+                this.markers[i].setVisible(false);
+            }
+        }
+        return result;
+    }, this);
+}
+
+googleError = function googleError() {
+    alert(
+        'Oops. An error occurred. Please refresh the page and try again!'
+    );
+};
+
+function startApp() {
+    ko.applyBindings(new ViewModel());
+}
